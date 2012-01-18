77fcc14486561429db28b0fc59ba0684b0f99bef
James Dunne <james.jdunne@gmail.com>
2012-01-18 12:26:16 -0600

    Included latest memory leak fixes for MyJIT.

--- jsdos/run.sh
+++ jsdos/run.sh
@@ -2,5 +2,5 @@
 IMGFILE="jsdos-`date +%Y%m%d`.img"
 
 pushd ../build &>/dev/null
-qemu-system-x86_64 -smp 4,cores=4 -no-kvm -hda "$IMGFILE" -name "JSDOS"
+qemu-system-x86_64 -smp 4,cores=4 -no-kvm -hda "$IMGFILE" -name "JSDOS" $@
 popd &>/dev/null
